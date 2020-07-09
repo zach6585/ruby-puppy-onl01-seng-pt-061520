8f92bf013693999f708019aeabef5e92c14f5a10
zachary mines <zmines@wesleyan.edu>
2020-07-09 18:45:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    self.save 
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.clear_all
+    @@all = []
+  end 
+  
+  def self.print_all
+    @@all.each do |deg|
+      puts deg.name 
+    end 
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+end 
